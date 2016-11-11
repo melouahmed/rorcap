922906e3204f14bf18b923fa3dac23c73c6a013e
Ismail Serhan <i.serhan@eutech-ssii.com>
2016-11-11 16:53:19 +0000

    samson deploy first try

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,69 @@ server '10.20.0.2', port: '22', roles: [:web, :app, :db], primary: true
 set :scm, :git
 set :application, 'rorcap'
 set :repo_url, 'git@github.com:serhanus7/rorcap.git'
-set :user, 'deploy'
+set :user, 'iserhan'
 set :use_sudo, false
 set :deploy_to, "/local/www/#{fetch(:application)}"
 set :deploy_vie, :remote_cache
+set :ssh_options,     { forward_agent: true, user: fetch(:user)}
+
+
+# set :repo_url,        'git@github.com:serhanus7/rorcap.git'
+# set :application,     'rorcap'
+# set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+#
+# # Don't change these unless you know what you're doing
+# set :pty,             true
+# set :use_sudo,        false
+set :stage,           :production
+# set :deploy_via,      :remote_cache
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/deploy.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+#
+# ## Defaults:
+# # set :scm,           :git
+# # set :branch,        :master
+# # set :format,        :pretty
+# # set :log_level,     :debug
+# # set :keep_releases, 5
+#
+# ## Linked Files & Directories (Default None):
+# # set :linked_files, %w{config/database.yml}
+# # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
   desc 'Initial Deploy'
   task :initial do
@@ -18,41 +75,19 @@ namespace :deploy do
     end
   end
 
-  task :bundle_gems do
-    run "cd #{fetch(:deploy_to)}/current && bundle install vendor/gems"
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
   end
 
-  before :starting,     :bundle_gems
+  before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
